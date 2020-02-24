c56705f7cc3cb3b1f566e1f07e10cce338d7dad3
ashanta-mgtech <55227867+ashanta-mgtech@users.noreply.github.com>
2020-02-24 16:46:43 +0530

    update

--- sqlbackup.sh
+++ sqlbackup.sh
@@ -2,4 +2,4 @@
 
 #To take the sql server backup for the all DB's
 
-sqlcmd -S localhost -U SA -Q "BACKUP DATABASE [Auricle] TO DISK = N'/var/opt/mssql/data/backup/Auricle.bak' WITH NOFORMAT, NOINIT, NAME = 'demodb-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
+sqlcmd -S localhost -U SA -Q "BACKUP DATABASE [Auricle] TO DISK = N'/var/opt/mssql/data/backup/Auricle.bak' WITH NOFORMAT, NOINIT, NAME = 'Auricle-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10"
